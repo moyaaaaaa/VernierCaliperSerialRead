c512a51dadd7f3ac9c8d1693e1e3f16450629e3e
moyaaaaaa <danmakubi@gmail.com>
2017-02-27 12:47:17 +0900

    削除機能実装（とりあえず）

---
+++ app/src/main/java/com/plantfactory/verniercaliperserialread/CustomListData.java
@@ -0,0 +1,17 @@
+package com.plantfactory.verniercaliperserialread;
+
+/**
+ * Created by Tomoya on 17/02/21.
+ */
+public class CustomListData {
+    private String textData;
+
+    public void setTextData(String textData)
+    {
+        this.textData = textData;
+    }
+    public String getTextData()
+    {
+        return textData;
+    }
+}
---
+++ app/src/main/java/com/plantfactory/verniercaliperserialread/MyListArrayAdapter.java
@@ -0,0 +1,7 @@
+package com.plantfactory.verniercaliperserialread;
+
+/**
+ * Created by Tomoya on 17/02/21.
+ */
+public class MyListArrayAdapter {
+}
